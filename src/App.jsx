ce1699b02da7bea13546eeced83db407a73bbcc2
MaRuF2131 <marufahmmed916@gmail.com>
2025-07-03 15:55:34 +0600

    basic setup of react query

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='w-10/12 h-96 bg-white mx-auto rounded-lg shadow-lg shadow-black'>
+      <h1 className='text-center text-3xl font-bold text-[#8fa3d0]'>useQuery Example</h1>
+    </div>
+   </>
+  )
+}
+
+export default App
